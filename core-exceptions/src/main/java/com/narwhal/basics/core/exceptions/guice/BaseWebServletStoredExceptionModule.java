bde6d05067f7c2edd8ada24cea3275d87c2f3b24
Tomas de Priede <tomasdepriede@gmail.com>
2017-10-25 04:46:46 -0300

    Fix version 1.0.8

--- core-exceptions/src/main/java/com/narwhal/basics/core/exceptions/guice/BaseWebServletStoredExceptionModule.java
+++ core-exceptions/src/main/java/com/narwhal/basics/core/exceptions/guice/BaseWebServletStoredExceptionModule.java
@@ -2,10 +2,15 @@ package com.narwhal.basics.core.exceptions.guice;
 
 import com.narwhal.basics.core.exceptions.mapper.StoredApiExceptionMapper;
 import com.narwhal.basics.core.rest.guice.BaseWebServletModule;
+import com.narwhal.basics.core.rest.guice.SubModule;
 
 import javax.ws.rs.ext.ExceptionMapper;
 
 public abstract class BaseWebServletStoredExceptionModule extends BaseWebServletModule {
+
+    public BaseWebServletStoredExceptionModule(SubModule... servletModuleList) {
+        super(servletModuleList);
+    }
 
     @Override
     protected Class<? extends ExceptionMapper> bindExceptionMapper() {
